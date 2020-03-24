59167e092e7716f28d619c9544875faf49d0979e
Andrew Howell <andhudhow@gmail.com>
2020-03-23 18:29:41 -0700

    set up skeleton

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: "./lib/rightbin.js",
+  output: {
+    path: path.resolve(__dirname),
+    filename: "./lib/bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
